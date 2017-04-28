49719b98df7dabebfc18e2dff1c7143d54a68a47
Ryan <ryanjonesirl@gmail.com>
2017-04-28 11:42:52 -0700

    add index route

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  },
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question')
+    });
+  }
+});
